ed6b774da8d2afc2b6e6934d7e3b165ab180f187
Namanh Tran <NamanhDTran@gmail.com>
2021-01-20 21:49:40 -0800

    search component now works just need to modualarize the component

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -8,7 +8,6 @@ import axios from 'axios';
 const Search = () => {
     const [text, setText] = useState('');
     const [dbText, setDbText] = useState(text);
-    const [movieData, setMovieData] = useState(null);
     const [options, setOptions] = useState([]);
 
     useEffect(() => {
@@ -24,61 +23,51 @@ const Search = () => {
     useEffect(() => {
         const search = async () => {
             const { data } = await axios.post('http://localhost:3100/search', {"query": dbText});
-
-            setMovieData(data.data);
-
-            const options_titles = [];
-            for (var i = 0; i < data.data.length; i++) {
-                options_titles.push(data.data[i].title);
-            }
-
-            setOptions(options_titles);
+           setOptions(data.data);
         };
         
         if (dbText) {
             search();
         }
 
-    }, [dbText]);
-
-    const onChange = (event) => {
-        setText(event.target.value);
-    }
-
-    const getImageLink = (title) => {
-        for (var i = 0; i < movieData.length; i++) {
-            if (movieData[i].title === title) {
-                return movieData[i].poster;
-            }
+        else {
+            setOptions([]);
         }
 
-        return null
-    };
+    }, [dbText]);
+
+    const onChange = (event, input) => {
+        setText(input);
+        console.log(input);
+    }
 
     const renderInput = (params) => {
-        return (<TextField {...params} id="standard-basic" label="Enter a Movie to Search" onChange={onChange} centered="true" fullWidth />);
+        return (<TextField {...params} id="standard-basic" label="Enter a Movie to Search" centered="true" fullWidth />);
     }
 
     const renderOption = (option) => {
         return (
             <Grid container spacing={2} alignItems="center" direction="row" className="image-result">
                 <Grid item xs={1}>
-                    <img src={`${getImageLink(option) !== null ? "https://image.tmdb.org/t/p/w500" + getImageLink(option) : ''}`} alt={option}/>
+                    <img src={`${option.poster ? "https://image.tmdb.org/t/p/w500" + option.poster : 'https://scontent-lax3-2.cdninstagram.com/v/t51.2885-19/s150x150/119710013_177484657221030_7662811337725111718_n.jpg?_nc_ht=scontent-lax3-2.cdninstagram.com&_nc_ohc=aiWg0C1jny0AX_UZYIC&tp=1&oh=116956072e4d080ea7c3664a0261e7bc&oe=60327946'}`} alt={option}/>
                 </Grid>
                 <Grid item xs>
-                    <Typography variant="h6">{option}</Typography>
+                    <Typography variant="h6">{option.title}</Typography>
                 </Grid>
             </Grid>
           );
     }
 
     return (
-        <Autocomplete  
-            value={text}
+        <Autocomplete
+            inputValue={text}
             options={options}
+            getOptionLabel={(option => option.title)}
+            getOptionSelected={(option, value) => option.title === value.title}
             renderInput={(params) => renderInput(params)}
             renderOption={renderOption}
-            getOptionSelected={() => true}
+            onInputChange={onChange}
+            freeSolo={true}
         />
     );
 };
