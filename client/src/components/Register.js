1f2af9a3028f36a40fbca88fea803f5fee686bcc
Namanh Tran <NamanhDTran@gmail.com>
2021-02-07 15:20:40 -0800

    sends signup request to the backend

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,14 +1,16 @@
-import { Button, Grid, Box, Typography } from '@material-ui/core';
+import { Button, Grid, Box, Typography, CircularProgress } from '@material-ui/core';
 import React, { useState } from 'react';
-import EmailValidator from 'email-validator';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 import CustomTextField from './CustomTextField';
 
-const Register = () => {
+const Register = ({ setLoggedIn }) => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
-    const [isUsernameValid, setIsUsernameValid] = useState(false);
-    const [isEmailValid, setIsEmailValid] = useState(false);
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const history = useHistory();
 
     const onUsernameChange = (event) => {
         console.log(event.target.value);
@@ -20,12 +22,25 @@ const Register = () => {
         setEmail(event.target.value);
     };
 
-    const onSubmit = (event) => {
-        if (isUsernameValid && isEmailValid) {
-            
-        }
+    const onPasswordChange = (event) => {
+        console.log(event.target.value);
+        setPassword(event.target.value);
+    }
 
+    const onSubmit = async (event) => {
         console.log("Submit button hit");
+        try {
+            setLoading(true);
+            const response = await axios.post('https://localhost:3100/signup', {'username': username, 'email': email, 'password': password}, {withCredentials: true});
+            setLoading(false);
+            console.log(response.headers);
+            setLoggedIn(true);
+            history.push("/");
+            
+        } catch (error) {
+            setLoading(false);
+            console.log('error', error);
+        }
     };
 
     return (
@@ -39,13 +54,13 @@ const Register = () => {
                         <CustomTextField required id="standard-required" label="Username" color="secondary" onChange={e => onUsernameChange(e)} />
                     </Grid>
                     <Grid item xs={12}>
-                        <CustomTextField required id="standard-required" label="Email" color="secondary" onChange={e => onEmailChange(e)}/>
+                        <CustomTextField required id="standard-required" label="Email" color="secondary" onChange={e => onEmailChange(e)} />
                     </Grid>
                     <Grid item xs={12}>
-                        <CustomTextField required id="standard-required-password" type="password" color="secondary" label="Password" />
+                        <CustomTextField required id="standard-required-password" type="password" color="secondary" label="Password" onChange={e => onPasswordChange(e)} />
                     </Grid>
                     <Grid item xs={12} pt={1}>
-                        <Button variant="contained" color="default" onClick={e => onSubmit(e)}>Create Account</Button>
+                        <Button variant="contained" color="default" onClick={e => onSubmit(e)}>{loading ? <CircularProgress color="inherit" size={20} /> : "Create Account"}</Button>
                     </Grid>
                 </Grid>
             </Box>
