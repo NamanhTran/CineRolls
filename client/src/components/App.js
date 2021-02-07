e7c7e246b91501766a87bd0a2ace1d6a2313416b
Namanh Tran <NamanhDTran@gmail.com>
2021-02-07 15:17:49 -0800

    now allows for session to be restored on refreshed or revisit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import Cookie from 'js-cookie';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 import NavBar from './NavBar';
 import Home from './Home';
@@ -8,16 +8,31 @@ import BoxBackground from './BoxBackground';
 import Search from './Search';
 import MovieDetail from './MovieDetails';
 import Login from './Login';
+import Logout from './Logout'
 import Register from './Register';
+
+axios.defaults.withCredentials = true;
 
 const App = () => {
     const [selection, setSelection] = useState('');
-    const [loggedIn, setLoggedIn] = useState(Cookie.get('login'));
+    const [hasAuth, setHasAuth] = useState(false);
+    const [user, setUser] = useState({username: 'Dummy Username'});
+
+    useEffect(() => {
+        const checkSession = async () => {
+            const { data } = await axios.get('https://localhost:3100/checkSession');
+            setHasAuth(data['isLoggedIn']);
+            setUser(data['user']);
+        }
+
+        // Check is session is valid if exists
+        checkSession();
+    }, []);
 
     return (
         <Router>
             <nav>
-                <NavBar loggedIn={loggedIn} />
+                <NavBar loggedIn={hasAuth} username={user.username} />
 
                 <Switch>
                     <Route path="/search">
@@ -29,19 +44,19 @@ const App = () => {
 
                     <Route path="/logout">
                         <BoxBackground>
-                            {"Log Out Page"}
+                            <Logout setLoggedIn={setHasAuth} setUser={setUser} />
                         </BoxBackground>
                     </Route>
 
                     <Route path="/login">
                         <BoxBackground maxWidth="xs">
-                            {loggedIn ? <Redirect to='/' /> : <Login setLoggedIn={setLoggedIn}/> }
+                            {hasAuth ? <Redirect to='/' /> : <Login setLoggedIn={setHasAuth} setUser={setUser}/> }
                         </BoxBackground>
                     </Route>
 
                     <Route path="/register">
                         <BoxBackground maxWidth="xs">
-                            {loggedIn ? null : <Register /> }
+                            {hasAuth ? null : <Register setLoggedIn={setHasAuth} setUser={setUser}/> }
                         </BoxBackground>
                     </Route>
 
